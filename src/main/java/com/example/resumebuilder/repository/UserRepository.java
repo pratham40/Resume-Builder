a1bfdaa3f55ca238bae107ae10a617ebdf94dbe5
pratham40 <pratham55575@gmail.com>
2025-12-25 22:26:23 +0530

    remove unused findByEmail method from UserRepository

--- src/main/java/com/example/resumebuilder/repository/UserRepository.java
+++ src/main/java/com/example/resumebuilder/repository/UserRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
 
-    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
